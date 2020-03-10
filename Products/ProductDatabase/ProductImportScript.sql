5f1e806f317d6ec229e041dc6361d46748d9fb42
Martin Vlček <martin.vlcek@certicon.cz>
2020-03-10 08:53:23 +0100

    update DB initial import script

--- Products/ProductDatabase/ProductImportScript.sql
+++ Products/ProductDatabase/ProductImportScript.sql
@@ -1,6 +1,9 @@
 ﻿/*
 Initial data import for dbo.Product table
 */
-INSERT INTO dbo.Product (Id, Name, ImgUri, Price) VALUES (1,'TestProduct1','TestUri1', 100)
-INSERT INTO dbo.Product (Id, Name, ImgUri, Price) VALUES (2,'TestProduct2','TestUri2', 200);
-INSERT INTO dbo.Product (Id, Name, ImgUri, Price) VALUES (3,'TestProduct3','TestUri3', 300);
+IF NOT EXISTS (SELECT * FROM dbo.Product WHERE Id = 1)
+	INSERT INTO dbo.Product (Id, Name, ImgUri, Price) VALUES (1,'TestProduct1','TestUri1', 100)
+IF NOT EXISTS (SELECT * FROM dbo.Product WHERE Id = 2)
+	INSERT INTO dbo.Product (Id, Name, ImgUri, Price) VALUES (2,'TestProduct2','TestUri2', 200);
+IF NOT EXISTS (SELECT * FROM dbo.Product WHERE Id = 3)
+	INSERT INTO dbo.Product (Id, Name, ImgUri, Price) VALUES (3,'TestProduct3','TestUri3', 300);
